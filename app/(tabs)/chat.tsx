4b2df31f25c0f67cf38c95272e158b6f2cb600e6
Bapparaj sk <bapparajsk97@gmail.com>
2025-10-16 20:08:22 +0530

    refactor(Chat): integrate Header component and enhance Wrapper layout with buttons for chat and room toggling

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,10 +1,13 @@
 import { Wrapper } from '@/components/chat';
+import Header from '@/components/ui/header';
+import { MessagesSquare } from 'lucide-react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Chat() {
     return (
-        <SafeAreaView style={{ flex: 1}}>
-            <Wrapper/>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Header icon={<MessagesSquare size={24} />} title="Chats" />
+            <Wrapper />
         </SafeAreaView>
     )
 }
--- components/chat/wrapper.tsx
+++ components/chat/wrapper.tsx
@@ -1,19 +1,39 @@
-import { MessagesSquare, Search, Video } from 'lucide-react-native';
-import React from 'react';
-import { View } from 'react-native';
+import { cn } from '@/utils/cn';
+import { MessageSquareText, MonitorPlay, Search, Video } from 'lucide-react-native';
+import { useState } from 'react';
+import { Text, View } from 'react-native';
 import AbsoluteButton from '../ui/absolute-button';
-import Header from '../ui/header';
+import { Button } from '../ui/button';
 import Input from '../ui/input';
 
+
 export default function Wrapper() {
+
+    const [isChats, setIsChats] = useState(true);
+
+    const handleToggle = (type: "chats" | "rooms") => setIsChats(type === "chats");
+
     return (
         <>
-            <Header icon={<MessagesSquare size={24} />} title="Chats" />
-            <View className="w-full h-20 flex items-center justify-center px-2">
+            <View className="w-full h-16 flex items-center justify-center px-2 pt-2">
                 <Input
                     startContent={<Search size={20} color="#555" />}
-                    placeholder='Search chat...'
+                    placeholder='Search chat, Room...'
                 />
+            </View>
+            <View className='w-full h-10 flex-row border-b border-default justify-start items-center'>
+                <Button onPress={() => handleToggle("chats")} size='sm' variant='light' className='h-8'>
+                    <View className='flex-row items-center gap-1'>
+                        <MessageSquareText size={16} strokeWidth={2.3} color={isChats ? "#3b82f6" : "#6b7280"} />
+                        <Text className={cn('text-base font-poppins-semibold', isChats ? "text-blue-500" : "text-gray-500")}>Chats</Text>
+                    </View>
+                </Button>
+                <Button onPress={() => handleToggle("rooms")} size='sm' variant='light' className='h-8'>
+                    <View className='flex-row items-center gap-1'>
+                        <MonitorPlay size={16} strokeWidth={2.3} color={isChats ? "#6b7280" : "#3b82f6"} />
+                        <Text className={cn('text-base font-poppins-semibold', isChats ? "text-gray-500" : "text-blue-500")}>Rooms</Text>
+                    </View>
+                </Button>
             </View>
             <AbsoluteButton icon={<Video color={"#ffffff"} />} />
         </>
